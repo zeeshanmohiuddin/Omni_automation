3724c855d3c75a2e974a83d181f52bf92fe84ac0
zeeshan.mohiuddin <zeeshan.mohiuddin@10.121.44.142>
2023-06-14 18:14:35 +0500

    convert to page object model

--- src/Omni_Automation_Package/Omni_Automation_class.java
+++ src/Omni_Automation_Package/Omni_Automation_class.java
@@ -7,18 +7,24 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Omni_Automation_class {
 
 	public static void main(String[] args) throws InterruptedException {
-		String Chrome_driver_path = "C:\\Users\\zeeshan.mohiuddin\\eclipse-workspace\\Omni_Automation\\Latest_chrome_driver\\chromedriver.exe";
-		System.setProperty("webdriver.chrome.driver",Chrome_driver_path);
-		ChromeDriver driver = new ChromeDriver();
-		driver.get("http://119.160.107.35:31007/apps/BBWebAgentAppBas/#/BBAuthMA/frmSignIn");
-		driver.manage().window().maximize();
-		Thread.sleep(2000);		
-		driver.findElement(By.xpath("//input[@kwp='frmSignIn_flxAlphaNum_txtBxField']")).sendKeys("03313055859");
-		driver.findElement(By.xpath("//input[@kwp='frmSignIn_flxAlphaNum1_txtBxField']")).sendKeys("070509");
-		Thread.sleep(2000);
-		driver.findElement(By.xpath("//button[@kwp='frmSignIn_btnContinue']")).click();
-		Thread.sleep(20000);
-		driver.close();
+//		String Chrome_driver_path = "C:\\Users\\zeeshan.mohiuddin\\eclipse-workspace\\Omni_Automation\\Latest_chrome_driver\\chromedriver.exe";
+//		System.setProperty("webdriver.chrome.driver",Chrome_driver_path);
+//		ChromeDriver driver = new ChromeDriver();
+//		driver.get("http://119.160.107.35:31007/apps/BBWebAgentAppBas/#/BBAuthMA/frmSignIn");
+//		driver.manage().window().maximize();
+//		Thread.sleep(2000);		
+//		driver.findElement(By.xpath("//input[@kwp='frmSignIn_flxAlphaNum_txtBxField']")).sendKeys("03313055859");
+//		driver.findElement(By.xpath("//input[@kwp='frmSignIn_flxAlphaNum1_txtBxField']")).sendKeys("070509");
+//		Thread.sleep(2000);
+//		
+//		for(int i=0;i<=15;i++) {
+//			driver.findElement(By.xpath("//button[@kwp='frmSignIn_btnContinue']")).click();
+//			System.out.println("click");
+//		}
+//		
+//		
+//		Thread.sleep(20000);
+//		driver.close();
 
 	}
 
