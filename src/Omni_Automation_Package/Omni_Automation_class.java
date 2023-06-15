79002774320e27c1b92925cb9d113e60f687ec48
SYSTEMS\zeeshan.mohiuddin <zeeshan.mohiuddin>
2023-06-15 19:49:37 +0500

    Read values from excel

--- src/Omni_Automation_Package/Omni_Automation_class.java
+++ src/Omni_Automation_Package/Omni_Automation_class.java
@@ -4,9 +4,21 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 public class Omni_Automation_class {
 
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) throws InterruptedException, IOException {
 //		String Chrome_driver_path = "C:\\Users\\zeeshan.mohiuddin\\eclipse-workspace\\Omni_Automation\\Latest_chrome_driver\\chromedriver.exe";
 //		System.setProperty("webdriver.chrome.driver",Chrome_driver_path);
 //		ChromeDriver driver = new ChromeDriver();
@@ -25,6 +37,30 @@ public class Omni_Automation_class {
 //		
 //		Thread.sleep(20000);
 //		driver.close();
+		
+		
+		
+		File src = new File("D:/Work/Automation/InputValues.xlsx");
+		FileInputStream fis = new FileInputStream(src); 
+		Workbook wb = WorkbookFactory.create(fis);
+		Sheet sheet0 = wb.getSheetAt(0);
+		Row row0 = sheet0.getRow(1);
+		Cell cell0 = row0.getCell(0);
+		System.out.println(cell0);
+		fis.close();
+//		
+
+//		XSSFWorkbook xsf = new XSSFWorkbook(fis);
+//		System.out.println("3");
+//		XSSFSheet sheet = xsf.getSheetAt(0);
+//		System.out.println("4");
+//		String entry1 = sheet.getRow(1).getCell(1).getStringCellValue();
+//		System.out.println("5");
+//		xsf.close();
+//		System.out.println("6");
+//		
+//		System.out.println(entry1);
+//		System.out.println("7");
 
 	}
 
--- src/test/login_test.java
+++ src/test/login_test.java
@@ -4,6 +4,17 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import pages.login_page;
 
@@ -11,7 +22,7 @@ public class login_test {
 	
 	private static WebDriver driver = null;
 
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) throws Exception {
 		String Chrome_driver_path = "C:\\Users\\zeeshan.mohiuddin\\eclipse-workspace\\Omni_Automation\\Latest_chrome_driver\\chromedriver.exe";
 		System.setProperty("webdriver.chrome.driver",Chrome_driver_path);
 		driver = new ChromeDriver();
@@ -28,10 +39,28 @@ public class login_test {
 
 	}
 	
-	public static void login_test() throws InterruptedException {
-		login_page.input_username(driver).sendKeys("03313055859");
+	public static void login_test() throws IOException, InterruptedException  { 
+		
+		
+		File src = new File("D:/Work/Automation/InputValues.xlsx");
+		FileInputStream fis = new FileInputStream(src); 
+		Workbook wb = WorkbookFactory.create(fis);
+		Sheet sheet0 = wb.getSheetAt(0);
+		Row row1 = sheet0.getRow(1);
+		Cell cell0 = row1.getCell(0);
+		
+		Cell cell1 = row1.getCell(1);
+		
+		String cell0_str = cell0.toString();
+		String cell1_str = cell1.toString();
+		System.out.println(cell0_str);
+		System.out.println(cell1_str);
+		fis.close();
+		
+		
+		login_page.input_username(driver).sendKeys(cell0_str);
 		Thread.sleep(2000);
-		login_page.input_password(driver).sendKeys("070509");
+		login_page.input_password(driver).sendKeys(cell1_str);
 		Thread.sleep(2000);
 		login_page.button_signin(driver).click();
 		Thread.sleep(20000);
